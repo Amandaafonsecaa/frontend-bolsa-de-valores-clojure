56edba3bbe5cf325d3f950e40c90e952ee5c7a1e
Amandaafonsecaa <amandafonsecarod09@gmail.com>
2025-12-05 09:07:11 -0300

    feat: help button component

---
+++ src/cljs/bolsa_front/components/help-button.cljs
@@ -0,0 +1,57 @@
+(ns bolsa-front.components.help-button
+  (:require [reagent.core :as r]))
+
+(defn help-button [titulo explicacao]
+  (let [modal-aberto (r/atom false)]
+    (fn []
+      [:div {:style {:position "relative"
+                     :display "inline-block"
+                     :cursor "pointer"}}
+       
+       ;; Botão "?" que abre/fecha
+       [:button
+        {:on-click #(swap! modal-aberto not)
+         :style {:background "none"
+                 :border "none"
+                 :color "#007bff"
+                 :font-weight "bold"
+                 :font-size "16px"
+                 :margin-left "8px"
+                 :padding "0"
+                 :cursor "pointer"}}
+        "ⓘ"]
+
+       (when @modal-aberto
+         [:div
+          {:on-click #(reset! modal-aberto false)
+           :style {:position "absolute"
+                   :top "100%"
+                   :left "50%"
+                   :transform "translateX(-50%)"
+                   :z-index "100"
+                   :width "300px"
+                   :background-color "#f0f8ff"
+                   :border "1px solid #007bff"
+                   :border-radius "8px"
+                   :padding "15px"
+                   :box-shadow "0 4px 12px rgba(0, 0, 0, 0.5)"
+                   :color "#2e2e2e"
+                   :text-align "left"
+                   :font-size "14px"}}
+
+          ;; Título da Explicação
+          [:div {:style {:font-weight "bold"
+                         :margin-top "5px"
+                         :color "#007bff"}}
+           titulo]
+
+          [:p explicacao]
+
+          ;; Botão de Fechar
+          [:button
+           {:on-click #(reset! modal-aberto false)
+            :style {:position "absolute" :top "5px" :right "5px"
+                    :background "none" :border "none" :font-weight "bold"
+                    :cursor "pointer" :color "#555"}}
+           "X"]
+          ])])))
